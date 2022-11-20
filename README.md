e083858cf53ef27530fa403a42ebfbc8ef0e5f4f
victorsantosdevops <victorls.eng@gmail.com>
2022-11-20 11:53:26 -0300

    Example for others

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# sd-api-rest-dao
+Instalar o Scoop
+----------------
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser # Optional: Needed to run a remote script the first time
+irm get.scoop.sh | iex
+
+Instalar o symfony-cli
+----------------------
+scoop install symfony-cli
+
+Instalar o Certificado do servidor
+----------------------------------
+
+symfony server:ca:install
+
+Rodar o banco de dados e o PHP My Admin
+---------------------------------------
+docker-compose up
+
+Rodar a Aplicação
+-----------------
+
+symfony server:start
---
+++ src/Entity/Pessoa.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PessoaRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PessoaRepository::class)]
+class Pessoa
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nome = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $codNac = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNome(): ?string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getCodNac(): ?string
+    {
+        return $this->codNac;
+    }
+
+    public function setCodNac(string $codNac): self
+    {
+        $this->codNac = $codNac;
+
+        return $this;
+    }
+}
